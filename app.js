c4df112bc7fa7180c25c240dd968074a31cdf38e
Sahil-1905 <sahilshorey2023@gmail.com>
2025-03-18 17:53:28 -0600

    Update

---
+++ app.js
@@ -0,0 +1,23 @@
+//this imports express  
+const express = require('express');
+
+//use dotenv to expose our environment variables
+require('dotenv').config();
+
+//we create a new instance of express as the app object 
+const app = express();
+//PORT assignment
+const PORT = process.env.PORT;
+
+//route handler that will define uri/url that does something
+app.get('/home', (req, res) =>{
+  res.send('Hello, World');
+});
+
+
+//start our server
+app.listen(PORT, () =>{
+    console.log(`Server started on port: ${PORT}`);
+});
+
+
